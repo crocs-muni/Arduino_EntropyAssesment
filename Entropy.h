79b176ac8bee1c255b3918b8badd46789c3ea824
LukeMcNemee <nemecl@seznam.cz>
2017-08-11 14:12:00 +0200

    header specified

--- Entropy.h
+++ Entropy.h
@@ -1,7 +1,31 @@
 #ifndef Entropy_h
 #define Entropy_h
 
-class
+//Max alphabet size should be 256
+
+class Entropy {
+
+public:
+  //convenience functions for input formatting
+  //maybe not necesary, depends on expected input
+  void convertToArray(char* input, char* output);
+
+  //run all tests and output the lowest value (safe estimate for min-entropy)
+  float allTests(char* input);
+
+  //individual tests
+  float mostCommonValueEstimate(char* input);
+  float collisionEstimate(char* input);
+  float markovEstimate(char* input);
+  float compressionEstimate(char* input);
+  float tTupleEstimate(char* input);
+  float lrsEstimate(char* input);
+
+private:
+  //number of bits used to represent sample output values
+  int bits_per_symbol = 8;
+
+}
 
 
 #endif
