a62bd853d37f3bdd5d395d41ebd91e1d2370a9cf
LukeMcNemee <nemecl@seznam.cz>
2017-08-11 14:18:29 +0200

    function headers copied to cpp file

---
+++ Entropy.cpp
@@ -0,0 +1,32 @@
+void Entropy::convertToArray(char* input, char* output){
+
+}
+
+float Entropy::allTests(char* input){
+
+}
+
+//individual tests
+float Entropy::mostCommonValueEstimate(char* input){
+
+}
+
+float Entropy::collisionEstimate(char* input){
+
+}
+
+float Entropy::markovEstimate(char* input){
+
+}
+
+float Entropy::compressionEstimate(char* input){
+
+}
+
+float Entropy::tTupleEstimate(char* input){
+
+}
+
+float Entropy::lrsEstimate(char* input){
+
+}
