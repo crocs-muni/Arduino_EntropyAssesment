c01688f91811d2279b118a471ef1514cbfeea57f
LukeMcNemee <nemecl@seznam.cz>
2017-09-05 10:50:35 +0200

    most common value completed

--- Entropy.cpp
+++ Entropy.cpp
@@ -1,4 +1,10 @@
 #include "Entropy.h"
+#include <iostream>
+#include <cstring>
+#include <math.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
 
 /*
 void Entropy::convertToArray(String input, char* output){
@@ -13,7 +19,33 @@ float Entropy::allTests(char* input, size_t len){
 //individual tests
 float Entropy::mostCommonValueEstimate(char* input, size_t len){
 
-  return 0;
+  char data[len];
+  memcpy(data, input, len*sizeof(char));
+
+  //sort for easy counting
+  sortArray(data, len);
+
+  char current = data[0];
+  int countCurrent = 1;
+  int totalCount = 1;
+  int cmax = 1;
+  for(int i = 1; i < len; i++){
+    if(data[i] == current){
+      countCurrent = countCurrent + 1;
+      if(countCurrent > cmax){
+        cmax = countCurrent;
+      }
+    } else {
+      current = data[i];
+      totalCount = totalCount + 1;
+      countCurrent = 1;
+    }
+  }
+  float pmax = (cmax * 1.0) / (len * 1.0);
+  float ubound = pmax + 2.576 * sqrt(pmax * (1.0-pmax)/(len * 1.0));
+  float pu = MIN(1,ubound);
+  float entropy = - log2( pu );
+  return entropy;
 }
 
 float Entropy::collisionEstimate(char* input, size_t len){
