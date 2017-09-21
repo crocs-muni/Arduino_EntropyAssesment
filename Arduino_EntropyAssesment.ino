8a1efe3d2d8387a50d92385c4e16e0239fe81b16
LukeMcNemee <nemecl@seznam.cz>
2017-09-21 15:00:01 +0200

    basic speed test

--- Arduino_EntropyAssesment.ino
+++ Arduino_EntropyAssesment.ino
@@ -2,12 +2,36 @@
 
 Entropy e;
 
+char string[501];
+unsigned long time1;
+unsigned long time2;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Program ready");
+  fillString();
+  Serial.println("String ready");
+  time1 = millis();
+  float minE = e.mostCommonValueEstimate(&string[0], 500);
+  time2 = millis();
+  Serial.println(minE);
+  Serial.println(time2-time1);
 
+  for(;;){
+    delay(1000);
+  }
+}
+
+void fillString(){
+   for(int i = 0; i < 500; i++){
+      string[i] = random(255);    
+      if(i % 100 == 0){
+         Serial.println(i);
+      }
+   }   
 }
